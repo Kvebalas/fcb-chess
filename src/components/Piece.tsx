5d1f64fb227f8d09c8fcf661f7aeee732cbc26ce
mindaugaswashere <sveiki@DESKTOP-LAM0UR.localdomain>
2020-10-10 11:00:59 +0300

    movable pieces, illegal to move on own pieces

--- src/components/Piece.tsx
+++ src/components/Piece.tsx
@@ -1,66 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Draggable from 'react-draggable';
-
 interface Props {
+  currentPieces: any,
   setActiveDrag: any,
   setNewPieces: any,
   piece: string,
   squareId: any
 }
 
-const Piece = ({setActiveDrag, setNewPieces, piece, squareId} : Props) => {
-  const [controlledPosition, setControlledPosition] = useState({x: 0, y:0})
-  const pieceRef: any = React.createRef();
 
+const Piece = ({currentPieces, setActiveDrag, setNewPieces, piece, squareId} : Props) => {
+  const boardOffsetTop: any = document.getElementById("thechess")?.offsetTop;
+  const pieceRef: any = React.createRef();
   const getDiff: any = (x: any, y: any, fromSquare: any) => {
     const letterArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
     const xDiff =  Math.floor(x / 75);
     const yDiff = 8 - Math.floor(y / 75);
     return `${letterArray[xDiff]}${yDiff}`;
   }
-
   useEffect(() => {
     // Update the document title using the browser API
     //console.log(pieceRef);
   });
-
-  const onStart = () => {
+  const onStart = (e: any, position: any) => {
     setActiveDrag(true);
-  };
-
-  const onControlledDrag = (e: any, position: any) => {
-    // const pieceName: string = pieceRef.current.id;
-    const {x, y} = position;
-    console.log(x,y);
-    setControlledPosition({x, y});
-  };
-
-  const onControlledDragStop = (e: any, position: any) => {
-    console.log('position', position);
     const pieceName: string = pieceRef.current.id;
-    onControlledDrag(e, position);
+    const fromWhichSquare: string = position.node.offsetParent.id;
+    console.log(`You currently moving ${pieceName} piece \nFrom ${fromWhichSquare} square.`);
+  };
+  const onStop = ({clientX, clientY}: any, position: any) => {
+    console.log('end');
+    const pieceName: string = pieceRef.current.id;
     const fromWhichSquare: any = position.node.offsetParent.id;
-    const {clientX, clientY} = e;
-    const {x,y} = position;
-    console.log(x,y)
-    const boardOffsetTop: any = document.getElementById("thechess")?.offsetTop;
     const toWhichSquare: any = getDiff(clientX, clientY-boardOffsetTop, fromWhichSquare);
     console.log({pieceName, fromWhichSquare, toWhichSquare})
-    setNewPieces(pieceName, fromWhichSquare, toWhichSquare);
+    if (currentPieces[toWhichSquare]?.startsWith(pieceName[0])) {
+      console.log("Illegal move")
+      setNewPieces(pieceName, fromWhichSquare, fromWhichSquare);
+    } else {
+      setNewPieces(pieceName, fromWhichSquare, toWhichSquare);
+    }
   };
-
-  const onStop = (pieceName: string, fromWhichSquare: string, toWhichSquare: string) => {
-  };
-
-  const dragHandlers = {onStart, onStop};
-
   return (
-    <Draggable position={controlledPosition} {...dragHandlers} onStop={(e, position) => onControlledDragStop(e, position)}>
+    <Draggable onStart={onStart} onStop={onStop}>
       <div id={piece} ref={pieceRef} className={`${piece ? `piece piece-${piece}` : ''}`} />
     </Draggable>
   );
 }
-
 
 
 export default Piece;
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import Piece from './Piece';
-
 interface Props {
+  currentPieces: object,
   setActiveDrag: any,
   setNewPieces: any,
   color: string,
   piece: string | null,
   id: 'string'
 }
-
-const Square = ({setActiveDrag, setNewPieces, color, piece, id} : Props) => {
+const Square = ({currentPieces, setActiveDrag, setNewPieces, color, piece, id} : Props) => {
   return (
     <div id={`${id}`} className={`chess-square color-${color}`}>
         <div className="chess-square-coord">{id}</div>
         {piece 
-          ? <Piece setActiveDrag={setActiveDrag} squareId={id} setNewPieces={setNewPieces} piece={piece} />
+          ? <Piece currentPieces={currentPieces} setActiveDrag={setActiveDrag} squareId={id} setNewPieces={setNewPieces} piece={piece} />
           : null
         } 
     </div> 
   );
 }
-
 export default Square
