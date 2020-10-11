da1ff13213559bee3b8196ae1b577f1eb577cea6
unknown <ciepa.ricardas@gmail.com>
2020-10-11 20:37:16 +0300

    play button

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,24 @@ import Blackcook from './assets/images/chess-images/abc.png';
 
 
 function App() {
+  const [ open, setOpen ] = React.useState(false);
+  const [isDisabled, setIsDisabled] = React.useState(true)
   return (
     <div className="main"> 
       <h2 className="centered-text">Mindes Chess!</h2>
+      <label htmlFor="">Password:</label>
+      <input onChange={()=>setIsDisabled(false)} type="text"/>
+      <br/>
+      <button disabled={isDisabled} onClick={()=> setOpen(true)} >Play!</button>
+      <br/>
+    
       <img src={Blackcook} alt="abc" />
-      <Board />
+      {
+        open
+          ? <Board />
+          : <div>Neteisingas slaptazodis</div>
+      }
+      
     </div>
   );
 }
