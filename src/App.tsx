ec9f60fb4fb8e559e456562ee04b2803948317cd
unknown <ciepa.ricardas@gmail.com>
2020-10-11 20:55:52 +0300

    password value, chess table animation

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,20 @@ import Blackcook from './assets/images/chess-images/abc.png';
 function App() {
   const [ open, setOpen ] = React.useState(false);
   const [isDisabled, setIsDisabled] = React.useState(true)
+  const validator = (e:any) => {
+    const inputValue=e.target.value;
+    if(inputValue==="sveiki")
+    {
+      setIsDisabled(false);
+    }
+    
+    
+  }
   return (
     <div className="main"> 
       <h2 className="centered-text">Mindes Chess!</h2>
       <label htmlFor="">Password:</label>
-      <input onChange={()=>setIsDisabled(false)} type="text"/>
+      <input onChange={(e)=>validator(e)} type="text"/>
       <br/>
       <button disabled={isDisabled} onClick={()=> setOpen(true)} >Play!</button>
       <br/>
